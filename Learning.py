738282ca3dc52e55e8b9307761f5baec1c27f45c
ICaleb78 <101569599+ICaleb78@users.noreply.github.com>
2024-10-21 12:40:05 +0100

    Update Learning.py

--- Learning.py
+++ Learning.py
@@ -330,4 +330,40 @@ rf_model.fit(train_X, train_y)
 rf_predictions_mae= rf_model.predict(val_X)
 rf_val_mae =mean_absolute_error(rf_predictions_mae, val_y)
 
+
 print("Validation MAE for Random Forest Model: {}".format(rf_val_mae))
+
+# Define the model. Set random_state to 1
+rf_model = RandomForestRegressor(random_state=1)
+
+# fit your model
+rf_model.fit(train_X, train_y)
+
+# Calculate the mean absolute error of your Random Forest model on the validation data
+rf_predictions_mae= rf_model.predict(val_X)
+rf_val_mae =mean_absolute_error(rf_predictions_mae, val_y)
+
+print("Validation MAE for Random Forest Model: {}".format(rf_val_mae))
+
+
+#Train a model for the competition
+#The code cell above trains a Random Forest model on train_X and train_y.
+#Use the code cell below to build a Random Forest model and train it on all of X and y
+
+# To improve accuracy, create a new Random Forest model which you will train on all training data
+rf_model_on_full_data = RandomForestRegressor()
+
+# fit rf_model_on_full_data on all data from the training data
+rf_model_on_full_data.fit(X, y)
+# path to file you will use for predictions
+test_data_path = '../input/test.csv'
+
+# read test data file using pandas
+test_data = pd.read_csv(test_data_path)
+
+# create test_X which comes from test_data but includes only the columns you used for prediction.
+# The list of columns is stored in a variable called features
+test_X = test_data[features]
+
+# make predictions which we will submit. 
+test_preds = rf_model_on_full_data.predict(test_X)

350952e0cd5267ae4cbc24f68f8dbcd46c1ebec0
ICaleb78 <101569599+ICaleb78@users.noreply.github.com>
2024-10-23 13:27:27 +0100

    Update Pandas.py

--- Pandas.py
+++ Pandas.py
@@ -149,3 +149,26 @@ bargain_wine = reviews.loc[bargain_idx, 'title']
 
 #q6: describing a bottle of wine. Is a wine more likely to be "tropical" or "fruity"? Create a Series descriptor_counts counting how many times each of these two words appears in the descriptio
 
+n_trop = reviews.description.map(lambda desc: "tropical" in desc).sum()
+n_fruity = reviews.description.map(lambda desc: "fruity" in desc).sum()
+descriptor_counts = pd.Series([n_trop, n_fruity], index=['tropical', 'fruity'])
+
+#q6We'd like to host these wine reviews on our website, but a rating system ranging from 80 to 100 points is too hard to understand - we'd like to translate them into simple star ratings. A score of 95 or higher counts as 3 stars, a score of at least 85 but less than 95 is 2 stars. Any other score is 1 star.
+
+#Also, the Canadian Vintners Association bought a lot of ads on the site, so any wines from Canada should automatically get 3 stars, regardless of points.
+
+#Create a series star_ratings with the number of stars corresponding to each review in the dataset.
+def stars(row):
+    if row.country == 'Canada':
+        return 3
+    elif row.points >= 95:
+        return 3
+    elif row.points >= 85:
+        return 2
+    else:
+        return 1
+    
+star_ratings = reviews.apply(stars, axis='columns')
+
+#One function we've been using heavily thus far is the value_counts() function. We can replicate what value_counts()
+reviews.groupby('points').points.count()

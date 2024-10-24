4ee3727ef829dd52d38031023ae44f36274bfdb2
ICaleb78 <101569599+ICaleb78@users.noreply.github.com>
2024-10-24 17:10:02 +0100

    Update Pandas.py

--- Pandas.py
+++ Pandas.py
@@ -220,3 +220,5 @@ orted_varieties = price_extremes.sort_values(by=['min', 'max'], ascending=False)
 #q5: Create a `Series` whose index is reviewers and whose values is the average review score given out by that reviewer. Hint: you will need the `taster_name` and `points` columns.
 reviewer_mean_ratings =reviews.groupby(['taster_name']).points.mean()
 #q6: What combination of countries and varieties are most common? Create a `Series` whose index is a `MultiIndex`of `{country, variety}` pairs. For example, a pinot noir produced in the US should map to `{"US", "Pinot Noir"}`. Sort the values in the `Series` in descending order based on wine count.
+country_variety_counts = reviews.groupby(['country', 'variety']).size().sort_values(ascending=False)
+

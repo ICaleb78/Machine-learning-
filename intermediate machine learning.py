e908c91382554d84c88eea661ed9c07a23efbeed
ICaleb78 <101569599+ICaleb78@users.noreply.github.com>
2024-10-28 13:59:07 +0100

    Update intermediate machine learning.py

--- intermediate machine learning.py
+++ intermediate machine learning.py
@@ -15,8 +15,7 @@ X = X_full[features].copy()
 X_test = X_test_full[features].copy()
 
 # Break off validation set from training data
-X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,
-                                                      random_state=0)
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,random_state=0)
 #The next code cell defines five different random forest models.  Run this code cell without changes.  (_To review **random forests**, look [here](https://www.kaggle.com/dansbecker/random-forests)._)
 from sklearn.ensemble import RandomForestRegressor
 
@@ -47,3 +46,45 @@ for i in range(0, len(models)):
 # Fill in the best model
 best_model = model_3
 #Q2 Generate test predictions:Great. You know how to evaluate what makes an accurate model. Now it's time to go through the modeling process and make predictions. In the line below, create a Random Forest model with the variable name `my_model`.
+# Define a model
+my_model = RandomForestRegressor(n_estimators=100, criterion='absolute_error', random_state=0)
+
+#Run the next code cell without changes.  The code fits the model to the training and validation data, and then generates test predictions that are saved to a CSV file.  These test predictions can be submitted directly to the competition!
+# Fit the model to the training data
+my_model.fit(X, y)
+
+# Generate test predictions
+preds_test = my_model.predict(X_test)
+
+# Save predictions in format used for competition scoring
+output = pd.DataFrame({'Id': X_test.index,'SalePrice': preds_test})
+output.to_csv('submission.csv', index=False)
+
+        #MISSING VALUES 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Load the data
+data = pd.read_csv('../input/melbourne-housing-snapshot/melb_data.csv')
+
+# Select target
+y = data.Price
+
+# To keep things simple, we'll use only numerical predictors
+melb_predictors = data.drop(['Price'], axis=1)
+X = melb_predictors.select_dtypes(exclude=['object'])
+
+# Divide data into training and validation subsets
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=0)
+
+#Define Function to Measure Quality of Each Approach
+#We define a function score_dataset() to compare different approaches to dealing with missing values. This function reports the mean absolute error (MAE) from a random forest model.
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+# Function for comparing different approaches
+def score_dataset(X_train, X_valid, y_train, y_valid):
+    model = RandomForestRegressor(n_estimators=10, random_state=0)
+    model.fit(X_train, y_train)
+    preds = model.predict(X_valid)
+    return mean_absolute_error(y_valid, preds)

a3f7fc2321690389b725c85c8a12e6ff31d0d996
ICaleb78 <101569599+ICaleb78@users.noreply.github.com>
2024-11-01 12:50:12 +0100

    Update intermediate machine learning.py

--- intermediate machine learning.py
+++ intermediate machine learning.py
@@ -307,7 +307,7 @@ from sklearn.preprocessing import OneHotEncoder
 
 # Apply one-hot encoder to each column with categorical data
 OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
-OH_cols_train = pd.DataFrame(OH_encoder.fit_transform(X_train[object_cols]))
+C
 OH_cols_valid = pd.DataFrame(OH_encoder.transform(X_valid[object_cols]))
 
 # One-hot encoding removed index; put it back
@@ -357,4 +357,73 @@ bad_label_cols = list(set(object_cols)-set(good_label_cols))
 print('Categorical columns that will be ordinal encoded:', good_label_cols)
 print('\nCategorical columns that will be dropped from the dataset:', bad_label_cols)
 
+#Part B
 
+#Use the next code cell to ordinal encode the data in `X_train` and `X_valid`.  Set the preprocessed DataFrames to `label_X_train` and `label_X_valid`, respectively.  
+#- We have provided code below to drop the categorical columns in `bad_label_cols` from the dataset. 
+#- You should ordinal encode the categorical columns in `good_label_cols`
+
+from sklearn.preprocessing import OrdinalEncoder
+
+# Drop categorical columns that will not be encoded
+label_X_train = X_train.drop(bad_label_cols, axis=1)
+label_X_valid = X_valid.drop(bad_label_cols, axis=1)
+
+# Apply ordinal encoder 
+ordinal_encoder = OrdinalEncoder()
+label_X_train[good_label_cols] = ordinal_encoder.fit_transform(X_train[good_label_cols])
+label_X_valid[good_label_cols] = ordinal_encoder.transform(X_valid[good_label_cols]) # Your code here
+
+
+#necessity of oneHot encoding 
+#this code is used to know the cardinality of categorical varaiable
+# Get number of unique entries in each column with categorical data
+object_nunique = list(map(lambda col: X_train[col].nunique(), object_cols))
+d = dict(zip(object_cols, object_nunique))
+
+# Print number of unique entries by column, in ascending order
+sorted(d.items(), key=lambda x: x[1])
+
+#As an example, consider a dataset with 10,000 rows, and containing one categorical column with 100 unique entries.
+
+#If this column is replaced with the corresponding one-hot encoding, how many entries are added to the dataset?
+#If we instead replace the column with the ordinal encoding, how many entries are added?
+
+# Fill in the line below: How many entries are added to the dataset by 
+# replacing the column with a one-hot encoding?
+OH_entries_added = 990000 # (10000 x 100) - 10000
+
+# Fill in the line below: How many entries are added to the dataset by
+# replacing the column with an ordinal encoding?
+label_entries_added = 0
+
+# Columns that will be one-hot encoded
+low_cardinality_cols = [col for col in object_cols if X_train[col].nunique() < 10]
+
+# Columns that will be dropped from the dataset
+high_cardinality_cols = list(set(object_cols)-set(low_cardinality_cols))
+
+print('Categorical columns that will be one-hot encoded:', low_cardinality_cols)
+print('\nCategorical columns that will be dropped from the dataset:', high_cardinality_cols)
+
+#final solution 
+# Apply one-hot encoder to each column with categorical data
+OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
+OH_cols_train = pd.DataFrame(OH_encoder.fit_transform(X_train[low_cardinality_cols]))
+OH_cols_valid = pd.DataFrame(OH_encoder.transform(X_valid[low_cardinality_cols]))
+
+# One-hot encoding removed index; put it back
+OH_cols_train.index = X_train.index
+OH_cols_valid.index = X_valid.index
+
+# Remove categorical columns (will replace with one-hot encoding)
+num_X_train = X_train.drop(object_cols, axis=1)
+num_X_valid = X_valid.drop(object_cols, axis=1)
+
+# Add one-hot encoded columns to numerical features
+OH_X_train = pd.concat([num_X_train, OH_cols_train], axis=1)
+OH_X_valid = pd.concat([num_X_valid, OH_cols_valid], axis=1)
+
+# Ensure all columns have string type
+OH_X_train.columns = OH_X_train.columns.astype(str)
+OH_X_valid.columns = OH_X_valid.columns.astype(str

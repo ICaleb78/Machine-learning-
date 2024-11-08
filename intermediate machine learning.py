233cefddbce988af0f21898155a02aa0ec5c6dd3
ICaleb78 <101569599+ICaleb78@users.noreply.github.com>
2024-11-08 15:43:12 +0100

    Update intermediate machine learning.py

--- intermediate machine learning.py
+++ intermediate machine learning.py
@@ -762,3 +762,72 @@ plt.show()
 #Step 3: Find the best parameter value
 #Given the results, which value for n_estimators seems best for the random forest model? Use your answer to set the value of n_estimators_best
 n_estimators_best = min(results, key=results.get)
+
+#XG BOOSTING :- GRADIENT BOOSTING (ENSEMBLE METHOD)
+#We begin by loading the training and validation data in X_train, X_valid, y_train, and y_valid
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Read the data
+data = pd.read_csv('../input/melbourne-housing-snapshot/melb_data.csv')
+
+# Select subset of predictors
+cols_to_use = ['Rooms', 'Distance', 'Landsize', 'BuildingArea', 'YearBuilt']
+X = data[cols_to_use]
+
+# Select target
+y = data.Price
+
+# Separate data into training and validation sets
+X_train, X_valid, y_train, y_valid = train_test_split(X, y)
+
+from xgboost import XGBRegressor #Input
+
+my_model = XGBRegressor()
+my_model.fit(X_train, y_train)
+
+#output
+#XGBRegressor(base_score=0.5, booster='gbtree', callbacks=None,
+             colsample_bylevel=1, colsample_bynode=1, colsample_bytree=1,
+             early_stopping_rounds=None, enable_categorical=False,
+             eval_metric=None, gamma=0, gpu_id=-1, grow_policy='depthwise',
+             importance_type=None, interaction_constraints='',
+             learning_rate=0.300000012, max_bin=256, max_cat_to_onehot=4,
+             max_delta_step=0, max_depth=6, max_leaves=0, min_child_weight=1,
+             missing=nan, monotone_constraints='()', n_estimators=100, n_jobs=0,
+             num_parallel_tree=1, predictor='auto', random_state=0, reg_alpha=0,
+             reg_lambda=1, ...)
+
+#We also make predictions and evaluate the model.
+
+from sklearn.metrics import mean_absolute_error
+
+predictions = my_model.predict(X_valid)
+print("Mean Absolute Error: " + str(mean_absolute_error(predictions, y_valid)))
+
+#Parameter Tunning of XGBoosoting 
+#1: n_estimators
+my_model = XGBRegressor(n_estimators=500)
+my_model.fit(X_train, y_train)
+
+#2: Modified n_estimators with early_stopping_rounds and the deteriorating number for the validation score using eval_set parameter
+my_model = XGBRegressor(n_estimators=500)
+my_model.fit(X_train, y_train, 
+             early_stopping_rounds=5, 
+             eval_set=[(X_valid, y_valid)],
+             verbose=False)
+
+
+#3:- #Modified n_estimators with early_stopping_rounds, the deteriorating number, and learning_rate
+my_model = XGBRegressor(n_estimators=1000, learning_rate=0.05)
+my_model.fit(X_train, y_train, 
+             early_stopping_rounds=5, 
+             eval_set=[(X_valid, y_valid)], 
+             verbose=False)
+
+#4:-  #Modified n_estimators with early_stopping_rounds, the deteriorating number, learning_rate and n_jobs( for parallelsim, usually for larger dataset not for small datatset)
+my_model = XGBRegressor(n_estimators=1000, learning_rate=0.05, n_jobs=4)
+my_model.fit(X_train, y_train, 
+             early_stopping_rounds=5, 
+             eval_set=[(X_valid, y_valid)], 
+             verbose=False)

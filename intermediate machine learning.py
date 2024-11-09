634f954c628f8b9df852647c1b12a55da8601379
ICaleb78 <101569599+ICaleb78@users.noreply.github.com>
2024-11-09 17:24:25 +0100

    Update intermediate machine learning.py

--- intermediate machine learning.py
+++ intermediate machine learning.py
@@ -831,3 +831,56 @@ my_model.fit(X_train, y_train,
              early_stopping_rounds=5, 
              eval_set=[(X_valid, y_valid)], 
              verbose=False)
+
+
+#EXERCISE 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Read the data
+X = pd.read_csv('../input/train.csv', index_col='Id')
+X_test_full = pd.read_csv('../input/test.csv', index_col='Id')
+
+# Remove rows with missing target, separate target from predictors
+X.dropna(axis=0, subset=['SalePrice'], inplace=True)
+y = X.SalePrice              
+X.drop(['SalePrice'], axis=1, inplace=True)
+
+# Break off validation set from training data
+X_train_full, X_valid_full, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,
+                                                                random_state=0)
+
+# "Cardinality" means the number of unique values in a column
+# Select categorical columns with relatively low cardinality (convenient but arbitrary)
+low_cardinality_cols = [cname for cname in X_train_full.columns if X_train_full[cname].nunique() < 10 and 
+                        X_train_full[cname].dtype == "object"]
+
+# Select numeric columns
+numeric_cols = [cname for cname in X_train_full.columns if X_train_full[cname].dtype in ['int64', 'float64']]
+
+# Keep selected columns only
+my_cols = low_cardinality_cols + numeric_cols
+X_train = X_train_full[my_cols].copy()
+X_valid = X_valid_full[my_cols].copy()
+X_test = X_test_full[my_cols].copy()
+
+# One-hot encode the data (to shorten the code, we use pandas)
+X_train = pd.get_dummies(X_train)
+X_valid = pd.get_dummies(X_valid)
+X_test = pd.get_dummies(X_test)
+X_train, X_valid = X_train.align(X_valid, join='left', axis=1)
+X_train, X_test = X_train.align(X_test, join='left', axis=1)
+
+#Step 1: Build model
+#Part A
+#In this step, you'll build and train your first model with gradient boosting.
+#Begin by setting my_model_1 to an XGBoost model. Use the XGBRegressor class, and set the random seed to 0 (random_state=0). Leave all other parameters as default.
+#Then, fit the model to the training data in X_train and y_train
+from xgboost import XGBRegressor
+# Define the model
+my_model_1 = XGBRegressor(random_state=0) # Your code here
+# Fit the model
+my_model_1.fit(X_train, y_train) # Your code here
+
+#Part B
+#Set predictions_1 to the model's predictions for the validation data. Recall that the validation features are stored in X_valid

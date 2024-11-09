5b4fe6ae71bece4101d9f93a7c8ef664ba9f2a27
ICaleb78 <101569599+ICaleb78@users.noreply.github.com>
2024-11-09 18:47:27 +0100

    Update intermediate machine learning.py

--- intermediate machine learning.py
+++ intermediate machine learning.py
@@ -938,3 +938,63 @@ mae_3 = (mean_absolute_error(predictions_3, y_valid)) # Your code here
 print("Mean Absolute Error:" , mae_3)
 
 #DATA LEAKAGE 
+import pandas as pd
+
+# Read the data
+data = pd.read_csv('../input/aer-credit-card-data/AER_credit_card_data.csv', 
+                   true_values = ['yes'], false_values = ['no'])
+
+# Select target
+y = data.card
+
+# Select predictors
+X = data.drop(['card'], axis=1)
+
+print("Number of rows in the dataset:", X.shape[0])
+X.head()
+#Since this is a small dataset, we will use cross-validation to ensure accurate measures of model quality
+from sklearn.pipeline import make_pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+
+# Since there is no preprocessing, we don't need a pipeline (used anyway as best practice!)
+my_pipeline = make_pipeline(RandomForestClassifier(n_estimators=100))
+cv_scores = cross_val_score(my_pipeline, X, y, 
+                            cv=5,
+                            scoring='accuracy')
+
+print("Cross-validation accuracy: %f" % cv_scores.mean())
+#Here is a summary of the data, which you can also find under the data tab:
+
+#card: 1 if credit card application accepted, 0 if not
+#reports: Number of major derogatory reports
+#age: Age n years plus twelfths of a year
+#income: Yearly income (divided by 10,000)
+#share: Ratio of monthly credit card expenditure to yearly income
+#expenditure: Average monthly credit card expenditure
+#owner: 1 if owns home, 0 if rents
+#selfempl: 1 if self-employed, 0 if not
+#dependents: 1 + number of dependents
+#months: Months living at current address
+#majorcards: Number of major credit cards held
+#active: Number of active credit accounts
+#A few variables look suspicious. For example, does expenditure mean expenditure on this card or on cards used before applying?
+#At this point, basic data comparisons can be very helpful
+expenditures_cardholders = X.expenditure[y]
+expenditures_noncardholders = X.expenditure[~y]
+
+print('Fraction of those who did not receive a card and had no expenditures: %.2f' \
+      %((expenditures_noncardholders == 0).mean()))
+print('Fraction of those who received a card and had no expenditures: %.2f' \
+      %(( expenditures_cardholders == 0).mean()))
+
+# Drop leaky predictors from dataset
+potential_leaks = ['expenditure', 'share', 'active', 'majorcards']
+X2 = X.drop(potential_leaks, axis=1)
+
+# Evaluate the model with leaky predictors removed
+cv_scores = cross_val_score(my_pipeline, X2, y, 
+                            cv=5,
+                            scoring='accuracy')
+
+print("Cross-val accuracy: %f" % cv_scores.mean()

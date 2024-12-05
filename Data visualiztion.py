06bb3fab89d05fa9bc4d47813bf25aa6daa40a89
ICaleb78 <101569599+ICaleb78@users.noreply.github.com>
2024-12-05 16:43:43 +0100

    Update Data visualiztion.py

--- Data visualiztion.py
+++ Data visualiztion.py
@@ -1,1 +1,82 @@
-nsdvnvkdsnlkn
+import pandas as pd
+pd.plotting.register_matplotlib_converters()
+import matplotlib.pyplot as plt
+%matplotlib inline
+import seaborn as sns
+print("Setup Complete")
+
+# Set up code checking
+import os
+if not os.path.exists("../input/ign_scores.csv"):
+    os.symlink("../input/data-for-datavis/ign_scores.csv", "../input/ign_scores.csv") 
+from learntools.core import binder
+binder.bind(globals())
+from learntools.data_viz_to_coder.ex3 import *
+print("Setup Complete")
+
+Plot data 
+# Set the width and height of the figure
+plt.figure(figsize=(16,6))
+
+# to add title to the chart 
+plt.title("i am confindence in GOD")
+
+# Line chart showing how FIFA rankings evolved over time 
+sns.lineplot(data=fifa_data)
+
+#Seabon exercise 
+# Path of the file to read
+fifa_filepath = "../input/fifa.csv"
+
+# Read the file into a variable fifa_data
+fifa_data = pd.read_csv(fifa_filepath, index_col="Date", parse_dates=True)
+
+# to print last rows
+# Print the last five rows of the data
+spotify_data.tail()
+
+#to print the name os columns in a dataset
+list(spotify_data.columns)
+
+
+# Set the width and height of the figure
+plt.figure(figsize=(14,6))
+
+# Add title
+plt.title("Daily Global Streams of Popular Songs in 2017-2018")
+
+# Line chart showing daily global streams of 'Shape of You'
+sns.lineplot(data=spotify_data['Shape of You'], label="Shape of You")
+
+# Line chart showing daily global streams of 'Despacito'
+sns.lineplot(data=spotify_data['Despacito'], label="Despacito")
+
+# Add label for horizontal axis
+plt.xlabel("Date")
+
+#BARCHART
+# Set the width and height of the figure
+plt.figure(figsize=(10,6))
+
+# Add title
+plt.title("Average Arrival Delay for Spirit Airlines Flights, by Month")
+
+# Bar chart showing average arrival delay for Spirit Airlines flights by month
+sns.barplot(x=flight_data.index, y=flight_data['NK'])
+
+# Add label for vertical axis
+plt.ylabel("Arrival delay (in minutes)")
+
+
+#HEATMAP
+# Set the width and height of the figure
+plt.figure(figsize=(14,7))
+
+# Add title
+plt.title("Average Arrival Delay for Each Airline, by Month")
+
+# Heatmap showing average arrival delay for each airline by month
+sns.heatmap(data=flight_data, annot=True)
+
+# Add label for horizontal axis
+plt.xlabel("Airline")

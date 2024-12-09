961aab61dd13fa2e3d005de8e8583490a1afa385
ICaleb78 <101569599+ICaleb78@users.noreply.github.com>
2024-12-09 15:08:46 +0100

    Update Data Cleaning.py

--- Data Cleaning.py
+++ Data Cleaning.py
@@ -7,3 +7,37 @@ nfl_data = pd.read_csv("../input/nflplaybyplay2009to2016/NFL Play by Play 2009-2
 
 # set seed for reproducibility
 np.random.seed(0)
+
+
+#HOW TO KNOW HOW MANY MISSING VALUE U 
+# get the number of missing data points per column
+missing_values_count = nfl_data.isnull().sum()
+
+# look at the # of missing points in the first ten columns
+missing_values_count[0:10]  
+
+# how many total missing values do we have?
+total_cells = np.product(nfl_data.shape)
+total_missing = missing_values_count.sum()
+
+# percent of data that is missing
+percent_missing = (total_missing/total_cells) * 100
+print(percent_missing)
+
+#Drop missing values
+
+# remove all the rows that contain a missing value
+nfl_data.dropna()  #this might probably remove all the data, if the dataset contains mostly missing data
+
+#this is better 
+# remove all columns with at least one missing value
+columns_with_na_dropped = nfl_data.dropna(axis=1)
+columns_with_na_dropped.head()
+
+# just how much data did we lose?
+print("Columns in original dataset: %d \n" % nfl_data.shape[1])
+print("Columns with na's dropped: %d" % columns_with_na_dropped.shape[1])
+
+# get a small subset of the NFL dataset
+subset_nfl_data = nfl_data.loc[:, 'EPA':'Season'].head()
+subset_nfl_data

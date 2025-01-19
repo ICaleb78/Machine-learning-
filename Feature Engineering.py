1c8b284c2b653a94919d2f5c8bdf0204e69346d3
ICaleb78 <101569599+ICaleb78@users.noreply.github.com>
2025-01-19 19:12:03 +0100

    Update Feature Engineering.py

--- Feature Engineering.py
+++ Feature Engineering.py
@@ -16,3 +16,20 @@ baseline_score = cross_val_score(
 baseline_score = -1 * baseline_score.mean()
 
 print(f"MAE Baseline Score: {baseline_score:.4}")
+
+X = df.copy()
+y = X.pop("CompressiveStrength")
+
+# Create synthetic features
+X["FCRatio"] = X["FineAggregate"] / X["CoarseAggregate"]
+X["AggCmtRatio"] = (X["CoarseAggregate"] + X["FineAggregate"]) / X["Cement"]
+X["WtrCmtRatio"] = X["Water"] / X["Cement"]
+
+# Train and score model on dataset with additional ratio features
+model = RandomForestRegressor(criterion="absolute_error", random_state=0)
+score = cross_val_score(
+    model, X, y, cv=5, scoring="neg_mean_absolute_error"
+)
+score = -1 * score.mean()
+
+print(f"MAE Score with Ratio Features: {score:.4}")

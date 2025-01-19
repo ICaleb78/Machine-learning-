09aa676d2e02718cbe1829aca78444bbce45e3cd
ICaleb78 <101569599+ICaleb78@users.noreply.github.com>
2025-01-19 18:54:49 +0100

    Add files via upload

---
+++ Feature Engineering.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_val_score
+
+df = pd.read_csv("../input/fe-course-data/concrete.csv")
+df.head()
+
+X = df.copy()
+y = X.pop("CompressiveStrength")
+
+# Train and score baseline model
+baseline = RandomForestRegressor(criterion="absolute_error", random_state=0)
+baseline_score = cross_val_score(
+    baseline, X, y, cv=5, scoring="neg_mean_absolute_error"
+)
+baseline_score = -1 * baseline_score.mean()
+
+print(f"MAE Baseline Score: {baseline_score:.4}")

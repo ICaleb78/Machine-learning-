13a59ccc7c754f930c40351b4236a72fdba0baa7
ICaleb78 <101569599+ICaleb78@users.noreply.github.com>
2025-02-17 10:57:05 +0100

    Update Feature Engineering.py

--- Feature Engineering.py
+++ Feature Engineering.py
@@ -33,3 +33,120 @@ score = cross_val_score(
 score = -1 * score.mean()
 
 print(f"MAE Score with Ratio Features: {score:.4}")
+
+
+#Mutual Information
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+plt.style.use("seaborn-whitegrid")
+
+df = pd.read_csv("../input/fe-course-data/autos.csv")
+df.head()
+
+#The scikit-learn algorithm for MI treats discrete features differently from continuous features. Consequently,
+#you need to tell it which are which. As a rule of thumb, anything that must have a float dtype is not discrete. 
+#Categoricals (object or categorial dtype) can be treated as discrete by giving them a label encoding. 
+#(You can review label encodings in our Categorical Variables lesson.)
+X = df.copy()
+y = X.pop("price")
+
+# Label encoding for categoricals
+for colname in X.select_dtypes("object"):
+    X[colname], _ = X[colname].factorize()
+
+# All discrete features should now have integer dtypes (double-check this before using MI!)
+discrete_features = X.dtypes == int
+
+from sklearn.feature_selection import mutual_info_regression
+
+def make_mi_scores(X, y, discrete_features):
+    mi_scores = mutual_info_regression(X, y, discrete_features=discrete_features)
+    mi_scores = pd.Series(mi_scores, name="MI Scores", index=X.columns)
+    mi_scores = mi_scores.sort_values(ascending=False)
+    return mi_scores
+
+mi_scores = make_mi_scores(X, y, discrete_features)
+mi_scores[::3]  # show a few features with their MI scores
+
+#And now a bar plot to make comparisions easier:
+def plot_mi_scores(scores):
+    scores = scores.sort_values(ascending=True)
+    width = np.arange(len(scores))
+    ticks = list(scores.index)
+    plt.barh(width, scores)
+    plt.yticks(width, ticks)
+    plt.title("Mutual Information Scores")
+
+
+plt.figure(dpi=100, figsize=(8, 5))
+plot_mi_scores(mi_scores)
+
+sns.relplot(x="curb_weight", y="price", data=df);
+sns.lmplot(x="horsepower", y="price", hue="fuel_type", data=df);
+
+#Exercise: Mutual Information
+# Setup feedback system
+from learntools.core import binder
+binder.bind(globals())
+from learntools.feature_engineering_new.ex2 import *
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.feature_selection import mutual_info_regression
+
+# Set Matplotlib defaults
+plt.style.use("seaborn-whitegrid")
+plt.rc("figure", autolayout=True)
+plt.rc(
+    "axes",
+    labelweight="bold",
+    labelsize="large",
+    titleweight="bold",
+    titlesize=14,
+    titlepad=10,
+)
+
+# Load data
+df = pd.read_csv("../input/fe-course-data/ames.csv")
+
+
+# Utility functions from Tutorial
+def make_mi_scores(X, y):
+    X = X.copy()
+    for colname in X.select_dtypes(["object", "category"]):
+        X[colname], _ = X[colname].factorize()
+    # All discrete features should now have integer dtypes
+    discrete_features = [pd.api.types.is_integer_dtype(t) for t in X.dtypes]
+    mi_scores = mutual_info_regression(X, y, discrete_features=discrete_features, random_state=0)
+    mi_scores = pd.Series(mi_scores, name="MI Scores", index=X.columns)
+    mi_scores = mi_scores.sort_values(ascending=False)
+    return mi_scores
+
+def plot_mi_scores(scores):
+    scores = scores.sort_values(ascending=True)
+    width = np.arange(len(scores))
+    ticks = list(scores.index)
+    plt.barh(width, scores)
+    plt.yticks(width, ticks)
+    plt.title("Mutual Information Scores")
+
+features = ["YearBuilt", "MoSold", "ScreenPorch"]
+sns.relplot(
+    x="value", y="SalePrice", col="variable", data=df.melt(id_vars="SalePrice", value_vars=features), facet_kws=dict(sharex=False),
+);
+X = df.copy()
+y = X.pop('SalePrice')
+
+mi_scores = make_mi_scores(X, y)
+print(mi_scores.head(20))
+# print(mi_scores.tail(20))  # uncomment to see bottom 20
+
+plt.figure(dpi=100, figsize=(8, 5))
+plot_mi_scores(mi_scores.head(20))
+# plot_mi_scores(mi_scores.tail(20))  # uncomment to see bottom 20

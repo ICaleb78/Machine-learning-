052e3e85a7e34dd6701fc174eda61671c02d57e6
ICaleb78 <101569599+ICaleb78@users.noreply.github.com>
2025-02-19 11:56:58 +0100

    Update Feature Engineering.py

--- Feature Engineering.py
+++ Feature Engineering.py
@@ -515,3 +515,240 @@ q_3.check()
 
 
 #Principal Component Analysis
+#The new features PCA constructs are actually just linear combinations (weighted sums) of the original features:
+#The new features PCA constructs are actually just linear combinations (weighted sums) of the original features:
+df["Size"] = 0.707 * X["Height"] + 0.707 * X["Diameter"]
+df["Shape"] = 0.707 * X["Height"] - 0.707 * X["Diameter"]
+
+#Example
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from IPython.display import display
+from sklearn.feature_selection import mutual_info_regression
+
+
+plt.style.use("seaborn-whitegrid")
+plt.rc("figure", autolayout=True)
+plt.rc(
+    "axes",
+    labelweight="bold",
+    labelsize="large",
+    titleweight="bold",
+    titlesize=14,
+    titlepad=10,
+)
+def plot_variance(pca, width=8, dpi=100):
+    # Create figure
+    fig, axs = plt.subplots(1, 2)
+    n = pca.n_components_
+    grid = np.arange(1, n + 1)
+    # Explained variance
+    evr = pca.explained_variance_ratio_
+    axs[0].bar(grid, evr)
+    axs[0].set(
+        xlabel="Component", title="% Explained Variance", ylim=(0.0, 1.0)
+    )
+    # Cumulative Variance
+    cv = np.cumsum(evr)
+    axs[1].plot(np.r_[0, grid], np.r_[0, cv], "o-")
+    axs[1].set(
+        xlabel="Component", title="% Cumulative Variance", ylim=(0.0, 1.0)
+    )
+    # Set up figure
+    fig.set(figwidth=8, dpi=100)
+    return axs
+    def make_mi_scores(X, y, discrete_features):
+    mi_scores = mutual_info_regression(X, y, discrete_features=discrete_features)
+    mi_scores = pd.Series(mi_scores, name="MI Scores", index=X.columns)
+    mi_scores = mi_scores.sort_values(ascending=False)
+    return mi_scores
+
+
+df = pd.read_csv("../input/fe-course-data/autos.csv")
+
+features = ["highway_mpg", "engine_size", "horsepower", "curb_weight"]
+
+X = df.copy()
+y = X.pop('price')
+X = X.loc[:, features]
+
+# Standardize
+X_scaled = (X - X.mean(axis=0)) / X.std(axis=0)
+
+from sklearn.decomposition import PCA
+
+# Create principal components
+pca = PCA()
+X_pca = pca.fit_transform(X_scaled)
+
+# Convert to dataframe
+component_names = [f"PC{i+1}" for i in range(X_pca.shape[1])]
+X_pca = pd.DataFrame(X_pca, columns=component_names)
+
+X_pca.head()
+
+#We'll wrap the loadings up in a dataframe
+loadings = pd.DataFrame(
+    pca.components_.T,  # transpose the matrix of loadings
+    columns=component_names,  # so the columns are the principal components
+    index=X.columns,  # and the rows are the original features
+)
+loadings
+# Look at explained variance
+plot_variance(pca);
+
+mi_scores = make_mi_scores(X_pca, y, discrete_features=False)
+mi_scores
+
+# Show dataframe sorted by PC3
+idx = X_pca["PC3"].sort_values(ascending=False).index
+cols = ["make", "body_style", "horsepower", "curb_weight"]
+df.loc[idx, cols]
+
+#To express this contrast, let's create a new ratio feature:
+df["sports_or_wagon"] = X.curb_weight / X.horsepower
+sns.regplot(x="sports_or_wagon", y='price', data=df, order=2);
+
+#Exercise: Principal Component Analysis
+# Setup feedback system
+from learntools.core import binder
+binder.bind(globals())
+from learntools.feature_engineering_new.ex5 import *
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import mutual_info_regression
+from sklearn.model_selection import cross_val_score
+from xgboost import XGBRegressor
+
+# Set Matplotlib defaults
+plt.style.use("seaborn-whitegrid")
+plt.rc("figure", autolayout=True)
+plt.rc(
+    "axes",
+    labelweight="bold",
+    labelsize="large",
+    titleweight="bold",
+    titlesize=14,
+    titlepad=10,
+)
+
+
+def apply_pca(X, standardize=True):
+    # Standardize
+    if standardize:
+        X = (X - X.mean(axis=0)) / X.std(axis=0)
+    # Create principal components
+    pca = PCA()
+    X_pca = pca.fit_transform(X)
+    # Convert to dataframe
+    component_names = [f"PC{i+1}" for i in range(X_pca.shape[1])]
+    X_pca = pd.DataFrame(X_pca, columns=component_names)
+    # Create loadings
+    loadings = pd.DataFrame(
+        pca.components_.T,  # transpose the matrix of loadings
+        columns=component_names,  # so the columns are the principal components
+        index=X.columns,  # and the rows are the original features
+    )
+    return pca, X_pca, loadings
+
+
+def plot_variance(pca, width=8, dpi=100):
+    # Create figure
+    fig, axs = plt.subplots(1, 2)
+    n = pca.n_components_
+    grid = np.arange(1, n + 1)
+    # Explained variance
+    evr = pca.explained_variance_ratio_
+    axs[0].bar(grid, evr)
+    axs[0].set(
+        xlabel="Component", title="% Explained Variance", ylim=(0.0, 1.0)
+    )
+    # Cumulative Variance
+    cv = np.cumsum(evr)
+    axs[1].plot(np.r_[0, grid], np.r_[0, cv], "o-")
+    axs[1].set(
+        xlabel="Component", title="% Cumulative Variance", ylim=(0.0, 1.0)
+    )
+    # Set up figure
+    fig.set(figwidth=8, dpi=100)
+    return axs
+
+
+def make_mi_scores(X, y):
+    X = X.copy()
+    for colname in X.select_dtypes(["object", "category"]):
+        X[colname], _ = X[colname].factorize()
+    # All discrete features should now have integer dtypes
+    discrete_features = [pd.api.types.is_integer_dtype(t) for t in X.dtypes]
+    mi_scores = mutual_info_regression(X, y, discrete_features=discrete_features, random_state=0)
+    mi_scores = pd.Series(mi_scores, name="MI Scores", index=X.columns)
+    mi_scores = mi_scores.sort_values(ascending=False)
+    return mi_scores
+
+
+def score_dataset(X, y, model=XGBRegressor()):
+    # Label encoding for categoricals
+    for colname in X.select_dtypes(["category", "object"]):
+        X[colname], _ = X[colname].factorize()
+    # Metric for Housing competition is RMSLE (Root Mean Squared Log Error)
+    score = cross_val_score(
+        model, X, y, cv=5, scoring="neg_mean_squared_log_error",
+    )
+    score = -1 * score.mean()
+    score = np.sqrt(score)
+    return score
+
+
+df = pd.read_csv("../input/fe-course-data/ames.csv")
+
+#Let's choose a few features that are highly correlated with our target, SalePrice
+features = [
+    "GarageArea",
+    "YearRemodAdd",
+    "TotalBsmtSF",
+    "GrLivArea",
+]
+
+print("Correlation with SalePrice:\n")
+print(df[features].corrwith(df.SalePrice))
+
+#Run this cell to apply PCA and extract the loadings
+X = df.copy()
+y = X.pop("SalePrice")
+X = X.loc[:, features]
+
+# `apply_pca`, defined above, reproduces the code from the tutorial
+pca, X_pca, loadings = apply_pca(X)
+print(loadings)
+
+
+#2) Create New Features
+#Add one or more new features to the dataset X. For a correct solution, get a validation score below 0.140 RMSLE. 
+#(If you get stuck, feel free to use the hint below!)
+
+Solution: Here are two possible solutions, though you might have been able to find others.
+
+# Solution 1: Inspired by loadings
+X = df.copy()
+y = X.pop("SalePrice")
+
+X["Feature1"] = X.GrLivArea + X.TotalBsmtSF
+X["Feature2"] = X.YearRemodAdd * X.TotalBsmtSF
+
+score = score_dataset(X, y)
+print(f"Your score: {score:.5f} RMSLE")
+
+
+# Solution 2: Uses components
+X = df.copy()
+y = X.pop("SalePrice")
+
+X = X.join(X_pca)
+score = score_dataset(X, y)
+print(f"Your score: {score:.5f} RMSLE")
